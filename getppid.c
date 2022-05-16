f7f6c4dbef53a532cc3151e729d05dc517ee08a3
Veralee225 <Veralee225@gmail.com>
2022-05-16 23:37:59 +0100

    changes

--- getppid.c
+++ getppid.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
-#include 
+#include <unistd.h>
+
+/**
+ * write a program that prints the parent process
+ * 
+ */
+int main()
+{
+    int pid, status, status2;
+    pid = fork();
+    switch (pid)
+}
